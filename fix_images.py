2c918e386ef524f64c09b3ad3fc72b5b65f4adb3
jkagub@gmail.com <jkagub@gmail.com>
2023-07-26 22:17:46 +0800

    moved the convert to RGB code inside the open image block

--- fix_images.py
+++ fix_images.py
@@ -12,19 +12,21 @@ def process_images(input_folder, output_folder):
         # Try if file is a valid image file
         try:
             with Image.open(image_path) as img:
-
+                # convert to RGB if necessary
+                if img.mode in ('RGBA', 'LA', 'P'):
+                    img = img.convert('RGB')
                 # Rotate the image 90° anti-clockwise
                 rotated_img = img.rotate(90, expand=True)
 
                 # Resize the image to 128x128
                 resized_img = rotated_img.resize((128, 128))
+                
         except:
             print(f"Error: {image_path} is not a valid image file")
             continue
 
         try:
-            if resized_img.mode in ('RGBA', 'LA', 'P'):
-                resized_img = img.convert('RGB')
+            
             # Save the image to the new folder with .jpeg extension
             #output_filename = os.path.splitext(filename)[0] + ".jpeg"
             output_path = os.path.join(output_folder, filename)
